a34254411784b4b9667f803b14527373825d6c24
Harshjan1915 <harshjangid085236@gmail.com>
2025-10-07 21:17:44 +0530

    Updated Calculator program as per review feedback

--- Calculator.c
+++ Calculator.c
@@ -1,84 +1,107 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <limits.h>
 
-int precedence(char op) {
-    if (op == '+' || op == '-') return 1;
-    if (op == '*' || op == '/') return 2;
-    return 0;
+#define MAX_EXPRESSION_SIZE 200
+#define MAX_STACK_SIZE 100
+
+int getOperatorPrecedence(char operatorSymbol) {
+    switch (operatorSymbol) {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/': return 2;
+        default: return 0;
+    }
 }
 
-int applyOp(int a, int b, char op, int *err) {
-    if (op == '+') return a + b;
-    if (op == '-') return a - b;
-    if (op == '*') return a * b;
-    if (op == '/') {
-        if (b == 0) {
-            *err = 1; // divide by zero
+int performOperation(int leftOperand, int rightOperand, char operatorSymbol, int *errorFlag) {
+    switch (operatorSymbol) {
+        case '+':
+            if (leftOperand > INT_MAX - rightOperand) {
+                *errorFlag = 1;
+                printf("Error: Integer overflow.\n");
+                return 0;
+            }
+            return leftOperand + rightOperand;
+        case '-':
+            return leftOperand - rightOperand;
+        case '*':
+            if (rightOperand != 0 && leftOperand > INT_MAX / rightOperand) {
+                *errorFlag = 1;
+                printf("Error: Integer overflow.\n");
+                return 0;
+            }
+            return leftOperand * rightOperand;
+        case '/':
+            if (rightOperand == 0) {
+                *errorFlag = 1;
+                printf("Error: Division by zero.\n");
+                return 0;
+            }
+            return leftOperand / rightOperand;
+        default:
+            return 0;
+    }
+}
+
+void processOperatorStack(int *numbers, int *numberTop, char *operators, int *operatorTop, int *errorFlag) {
+    int rightOperand = numbers[(*numberTop)--];
+    int leftOperand = numbers[(*numberTop)--];
+    char operatorSymbol = operators[(*operatorTop)--];
+    int result = performOperation(leftOperand, rightOperand, operatorSymbol, errorFlag);
+    numbers[++(*numberTop)] = result;
+}
+
+int evaluateExpression(const char *expression, int *errorFlag) {
+    int numbers[MAX_STACK_SIZE];
+    char operators[MAX_STACK_SIZE];
+    int numberTop = -1, operatorTop = -1;
+
+    for (int i = 0; expression[i] != '\0'; i++) {
+        if (isspace(expression[i])) continue;
+
+        if (isdigit(expression[i])) {
+            int number = 0;
+            while (isdigit(expression[i])) {
+                number = number * 10 + (expression[i] - '0');
+                i++;
+            }
+            i--;
+            numbers[++numberTop] = number;
+        } else if (expression[i] == '+' || expression[i] == '-' ||
+                   expression[i] == '*' || expression[i] == '/') {
+            while (operatorTop >= 0 &&
+                   getOperatorPrecedence(operators[operatorTop]) >= getOperatorPrecedence(expression[i])) {
+                processOperatorStack(numbers, &numberTop, operators, &operatorTop, errorFlag);
+            }
+            operators[++operatorTop] = expression[i];
+        } else {
+            *errorFlag = 1;
+            printf("Error: Invalid character '%c' in expression.\n", expression[i]);
             return 0;
         }
-        return a / b; 
     }
-    return 0;
+
+    while (operatorTop >= 0) {
+        processOperatorStack(numbers, &numberTop, operators, &operatorTop, errorFlag);
+    }
+
+    return numbers[numberTop];
 }
 
 int main() {
-    char exp[200];
+    char expression[MAX_EXPRESSION_SIZE];
     printf("Enter expression: ");
-    fgets(exp, sizeof(exp), stdin);
+    fgets(expression, sizeof(expression), stdin);
 
-    int numStack[100], nsTop = -1;
-    char opStack[100]; int osTop = -1;
-    int i = 0, error = 0;
+    int errorFlag = 0;
+    int result = evaluateExpression(expression, &errorFlag);
 
-    while (exp[i] != '\0' && exp[i] != '\n') {
-        if (exp[i] == ' ') {
-            i++;
-            continue;
-        }
-
-        if (isdigit(exp[i])) {
-            int val = 0;
-            while (isdigit(exp[i])) {
-                val = val * 10 + (exp[i] - '0');
-                i++;
-            }
-            numStack[++nsTop] = val;
-            continue;
-        }
-        else if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') {
-            while (osTop != -1 && precedence(opStack[osTop]) >= precedence(exp[i])) {
-                int b = numStack[nsTop--];
-                int a = numStack[nsTop--];
-                char op = opStack[osTop--];
-                int res = applyOp(a, b, op, &error);
-                if (error) {
-                    printf("Error: Division by zero.\n");
-                    return 0;
-                }
-                numStack[++nsTop] = res;
-            }
-            opStack[++osTop] = exp[i];
-        }
-        else {
-            printf("Error: Invalid expression.\n");
-            return 0;
-        }
-        i++;
+    if (!errorFlag) {
+        printf("Result: %d\n", result);
     }
 
-    while (osTop != -1) {
-        int b = numStack[nsTop--];
-        int a = numStack[nsTop--];
-        char op = opStack[osTop--];
-        int res = applyOp(a, b, op, &error);
-        if (error) {
-            printf("Error: Division by zero.\n");
-            return 0;
-        }
-        numStack[++nsTop] = res;
-    }
-
-    printf("%d\n", numStack[nsTop]);
     return 0;
 }
